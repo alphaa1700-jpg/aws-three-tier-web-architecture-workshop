99bb375b6aff1f77085bf129782915b9546aaa88
Alpha <alphaa1700@gmail.com>
2025-12-04 15:10:55 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     DB_HOST : 'database-1.cluster-cvisecq8svuh.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'Welcome-db',
+    DB_PWD : 'welcome-db',
     DB_DATABASE : 'webappdb'
 });
